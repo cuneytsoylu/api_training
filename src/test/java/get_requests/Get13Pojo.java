244a5fe19aa22c4eb919cf5d0ad0e13991a73034
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-11-09 14:07:09 +0300

    09112022

--- src/test/java/get_requests/Get13Pojo.java
+++ src/test/java/get_requests/Get13Pojo.java
@@ -5,13 +5,12 @@ import io.restassured.response.Response;
 import org.junit.Test;
 import pojos.GoRestDataPojo;
 import pojos.GoRestPojo;
-import test_data.GoRestTestData;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class Get13Pojo extends GoRestBaseUrl {
-
-    /*
+     /*
         Given
             https://gorest.co.in/public/v1/users/2508
         When
@@ -33,24 +32,29 @@ public class Get13Pojo extends GoRestBaseUrl {
     */
 
     @Test
-    public void get13Pojo() {
+    public void get13Pojo(){
         //Set the Url
         spec.pathParams("first","users","second",2508);
 
         //Set the Expected Data
-        GoRestDataPojo goRestDataPojo =new GoRestDataPojo(2508,"Sharmila Deshpande VM","deshpande_sharmila_vm@becker.name","female","active");
+        GoRestDataPojo goRestDataPojo = new GoRestDataPojo(2508,"Sharmila Deshpande VM","deshpande_sharmila_vm@becker.name","female","active");
         GoRestPojo expectedData = new GoRestPojo(null,goRestDataPojo);
         System.out.println("expectedData = " + expectedData);
 
         //Send the Request and Get the Response
-        Response response =given().spec(spec).when().get("/{first}/{second}");
+        Response response = given().spec(spec).when().get("/{first}/{second}");
         response.prettyPrint();
 
         //Do Assertion
-       GoRestPojo actualData = response.as(GoRestPojo.class);
+        GoRestPojo actualData = response.as(GoRestPojo.class);
+        System.out.println("actualData = " + actualData);
 
-
-
-
+        assertEquals(200,response.getStatusCode());
+        assertEquals(expectedData.getMeta(),actualData.getMeta());
+        assertEquals(goRestDataPojo.getId(), actualData.getData().getId());
+        assertEquals(goRestDataPojo.getName(), actualData.getData().getName());
+        assertEquals(goRestDataPojo.getEmail(), actualData.getData().getEmail());
+        assertEquals(goRestDataPojo.getGender(), actualData.getData().getGender());
+        assertEquals(goRestDataPojo.getStatus(), actualData.getData().getStatus());
     }
 }
